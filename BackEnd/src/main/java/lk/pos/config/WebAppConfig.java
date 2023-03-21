9834329ce29b39c7d6d9a6052a666d61f7ee4a34
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 20:19:52 +0530

    Update WebAppConfig

--- BackEnd/src/main/java/lk/pos/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/pos/config/WebAppConfig.java
@@ -1,5 +1,6 @@
 package lk.pos.config;
 
+import lk.pos.advisor.AppWideExceptionHandler;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
