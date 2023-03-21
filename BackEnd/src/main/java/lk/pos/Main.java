b755ce75e7aa582725a394cf3748b9296e750d9c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 19:58:20 +0530

    Update Content

---
+++ BackEnd/src/main/java/lk/pos/Main.java
@@ -0,0 +1,11 @@
+package lk.pos;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
