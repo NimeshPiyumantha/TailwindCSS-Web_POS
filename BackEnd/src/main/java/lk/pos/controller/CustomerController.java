2a023a24cf6034071c4056fedf7f071e44567178
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 23:12:35 +0530

    Add CustomerController to annotation and mapping

--- BackEnd/src/main/java/lk/pos/controller/CustomerController.java
+++ BackEnd/src/main/java/lk/pos/controller/CustomerController.java
@@ -1,8 +1,15 @@
 package lk.pos.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
+@RestController
+@CrossOrigin
+@RequestMapping("/customer")
 public class CustomerController {
 }
