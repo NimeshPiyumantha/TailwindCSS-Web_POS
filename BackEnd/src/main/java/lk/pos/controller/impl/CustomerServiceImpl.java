52c12093fa0f4baa558b792b72fb017fc437e927
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 23:06:53 +0530

    Add CustomerServiceImpl

---
+++ BackEnd/src/main/java/lk/pos/controller/impl/CustomerServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.pos.controller.impl;
+
+import lk.pos.controller.CustomerService;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class CustomerServiceImpl implements CustomerService {
+}
