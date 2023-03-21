e2f3649a5af7106e7af39252ac24753dad404e7e
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 23:17:37 +0530

    Add CustomerController to CustomerServiceImpl annotation

--- BackEnd/src/main/java/lk/pos/service/impl/CustomerServiceImpl.java
+++ BackEnd/src/main/java/lk/pos/service/impl/CustomerServiceImpl.java
@@ -1,6 +1,9 @@
 package lk.pos.service.impl;
 
+import lk.pos.repo.CustomerRepo;
 import lk.pos.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepo repo;
+    @Autowired
+    private ModelMapper mapper;
 }
