81474d2591a2df1f9a994b17763daa3c131391ab
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 20:08:01 +0530

    Update WebAppInitializer

--- BackEnd/src/main/java/lk/pos/WebAppInitializer.java
+++ BackEnd/src/main/java/lk/pos/WebAppInitializer.java
@@ -4,7 +4,7 @@ package lk.pos;
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
-public class Main {
+public class WebAppInitializer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
