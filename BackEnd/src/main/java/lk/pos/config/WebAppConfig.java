226db4f4d1900dbde672356880705207b4ab1724
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 20:11:03 +0530

    Add WebAppConfig

---
+++ BackEnd/src/main/java/lk/pos/config/WebAppConfig.java
@@ -0,0 +1,15 @@
+package lk.pos.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {AppWideExceptionHandler.class}, basePackages = "lk.pos.controller")
+public class WebAppConfig {
+}
