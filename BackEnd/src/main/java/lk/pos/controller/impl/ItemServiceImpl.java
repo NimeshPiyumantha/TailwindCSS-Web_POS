b4be6d505f159f8837f70476f4046a52a5f935db
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 23:07:48 +0530

    Add ItemServiceImpl

---
+++ BackEnd/src/main/java/lk/pos/controller/impl/ItemServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.pos.controller.impl;
+
+import lk.pos.controller.ItemService;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class ItemServiceImpl implements ItemService {
+}
