96a1e218c581d84f7b23ecb792450230e129abad
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-21 23:09:17 +0530

    Add CustomerServiceImpl to annotations

--- BackEnd/src/main/java/lk/pos/controller/impl/CustomerServiceImpl.java
+++ BackEnd/src/main/java/lk/pos/controller/impl/CustomerServiceImpl.java
@@ -1,10 +1,14 @@
 package lk.pos.controller.impl;
 
 import lk.pos.controller.CustomerService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
 }
