fdbf89365dd24506219a148cd578dc5a4fe7376a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-22 23:11:53 +0530

    Add saveItem ItemService

--- BackEnd/src/main/java/lk/pos/service/ItemService.java
+++ BackEnd/src/main/java/lk/pos/service/ItemService.java
@@ -1,8 +1,12 @@
 package lk.pos.service;
 
+import lk.pos.dto.ItemDTO;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public interface ItemService {
+    void saveItem(@ModelAttribute ItemDTO dto);
 }
