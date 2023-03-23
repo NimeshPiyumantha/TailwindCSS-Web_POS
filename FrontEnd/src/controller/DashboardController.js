542ececd5d6bb1da5220b896ca13bc71cd4a68c7
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-23 22:51:15 +0530

    Update Project

--- FrontEnd/src/controller/DashboardController.js
+++ FrontEnd/src/controller/DashboardController.js
@@ -5,3 +5,45 @@
  * @date : 3/22/2023
  * @time : 11:50 PM
  **/
+
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open');
+    nav.classList.toggle('flex');
+    nav.classList.toggle('hidden');
+});
+
+$(document).ready(function () {
+    $("#HomeSection").css('display', 'block');
+    $("#CustomerSection").css('display', 'none');
+    $("#ItemSection").css('display', 'none');
+    $("#OrderSection").css('display', 'none');
+    $("#OrderDetails").css('display', 'none');
+});
+
+$("#homeBtn,.homeBtn").click(function () {
+    $("#HomeSection").css('display', 'block');
+    $("#CustomerSection").css('display', 'none');
+    $("#ItemSection").css('display', 'none');
+    $("#OrderSection").css('display', 'none');
+    $("#OrderDetails").css('display', 'none');
+});
+
+
+$("#customerBtn,.customerBtn").click(function () {
+    $("#HomeSection").css('display', 'none');
+    $("#CustomerSection").css('display', 'block');
+    $("#ItemSection").css('display', 'none');
+    $("#OrderSection").css('display', 'none');
+    $("#OrderDetails").css('display', 'none');
+});
+
+$("#itemBtn,.itemBtn").click(function () {
+    $("#HomeSection").css('display', 'none');
+    $("#CustomerSection").css('display', 'none');
+    $("#ItemSection").css('display', 'block');
+    $("#OrderSection").css('display', 'none');
+    $("#OrderDetails").css('display', 'none');
+});
