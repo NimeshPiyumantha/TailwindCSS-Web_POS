81f5c8334ae3056ab84c930e48659087629a7b61
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-23 08:15:19 +0530

    Add generateItemID ItemController

--- FrontEnd/src/controller/ItemController.js
+++ FrontEnd/src/controller/ItemController.js
@@ -12,4 +12,32 @@ loadAllItems();
 $("#btnAddItem").attr('disabled', true);
 $("#btnUpdateItem").attr('disabled', true);
 $("#btnDeleteItem").attr('disabled', true);
+/**
+ * Item Save
+ * Item ID
+ * */
+generateItemID();
+function generateItemID() {
+    $("#txtItemID").val("I00-001");
+    $.ajax({
+        url: baseUrlItem + "item/ItemIdGenerate",
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let code = resp.value;
+            let tempId = parseInt(code.split("-")[1]);
+            tempId = tempId + 1;
+            if (tempId <= 9) {
+                $("#txtItemID").val("I00-00" + tempId);
+            } else if (tempId <= 99) {
+                $("#txtItemID").val("I00-0" + tempId);
+            } else {
+                $("#txtItemID").val("I00-" + tempId);
+            }
+        },
+        error: function (ob, statusText, error) {
 
+        }
+    });
+}
