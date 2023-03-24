e6eacdd81595a04528f480661b48eab34c16a245
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-24 19:21:29 +0530

    Add placeOrder

--- BackEnd/src/main/java/lk/pos/service/PlaceOrderService.java
+++ BackEnd/src/main/java/lk/pos/service/PlaceOrderService.java
@@ -1,10 +1,14 @@
 package lk.pos.service;
 
 
+import lk.pos.dto.OrdersDTO;
+import org.springframework.web.bind.annotation.RequestBody;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public interface PlaceOrderService {
+    void placeOrder(@RequestBody OrdersDTO dto);
 
 }
