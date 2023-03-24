9ff9ebe9665a3c149fd5474afc34f21aa36829ba
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-24 19:26:06 +0530

    Add PlaceOrderServiceImpl implements PlaceOrderService

---
+++ BackEnd/src/main/java/lk/pos/service/impl/PlaceOrderServiceImpl.java
@@ -0,0 +1,13 @@
+package lk.pos.service.impl;
+
+
+import lk.pos.service.PlaceOrderService;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+public class PlaceOrderServiceImpl implements PlaceOrderService {
+
+}
