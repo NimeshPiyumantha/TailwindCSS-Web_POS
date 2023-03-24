fd32429c5b782f99a325b6331c0ab3340235b229
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-24 19:22:09 +0530

    Add PlaceOrderService LoadOrders

--- BackEnd/src/main/java/lk/pos/service/PlaceOrderService.java
+++ BackEnd/src/main/java/lk/pos/service/PlaceOrderService.java
@@ -4,11 +4,14 @@ package lk.pos.service;
 import lk.pos.dto.OrdersDTO;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public interface PlaceOrderService {
     void placeOrder(@RequestBody OrdersDTO dto);
+    ArrayList<OrdersDTO> LoadOrders();
 
 }
