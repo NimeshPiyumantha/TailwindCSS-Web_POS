1f55d8f33be099baec7b0ca0e14fcff2332da5e8
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-24 19:11:49 +0530

    Add OrderDTO Entity

---
+++ BackEnd/src/main/java/lk/pos/dto/OrdersDTO.java
@@ -0,0 +1,22 @@
+package lk.pos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrdersDTO {
+    private String oid;
+    private String date;
+    private String cusID;
+    private List<OrderDetailsDTO> orderDetails;
+
+}
