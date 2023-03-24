0d78ea16d12b63aeb74c118eb29091defa5d4490
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-24 19:09:25 +0530

    Add Order Entity

---
+++ BackEnd/src/main/java/lk/pos/entity/Orders.java
@@ -0,0 +1,30 @@
+package lk.pos.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Orders {
+    @Id
+    private String oid;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "customerID", referencedColumnName = "id", nullable = false)
+    private Customer cusID;
+    private String date;
+
+    //Inverse
+    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL)
+    private List<OrderDetails> orderDetails;
+}
