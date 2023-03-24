264c3baa46f8549218030c55d4c210c569894c04
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-24 19:26:44 +0530

    Update Content

--- BackEnd/src/main/java/lk/pos/service/impl/PlaceOrderServiceImpl.java
+++ BackEnd/src/main/java/lk/pos/service/impl/PlaceOrderServiceImpl.java
@@ -2,12 +2,16 @@ package lk.pos.service.impl;
 
 
 import lk.pos.service.PlaceOrderService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
-
+@Service
+@Transactional
 public class PlaceOrderServiceImpl implements PlaceOrderService {
 
 }
