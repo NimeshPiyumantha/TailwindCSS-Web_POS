babd2659a519485dfcce1e1472f9b3e2a193d83d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-24 19:34:00 +0530

    Add PlaceOrderController

---
+++ BackEnd/src/main/java/lk/pos/controller/PlaceOrderController.java
@@ -0,0 +1,15 @@
+package lk.pos.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+public class PlaceOrderController {
+
+}
