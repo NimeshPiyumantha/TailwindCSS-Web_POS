ed931d0a20efdf294c8df49eba48efedf41f9a50
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-27 12:32:36 +0530

    Add Date Default code

--- FrontEnd/src/controller/PurchaseOrderController.js
+++ FrontEnd/src/controller/PurchaseOrderController.js
@@ -329,4 +329,20 @@ $(document).on("change keyup blur", "#txtCash", function () {
     }
 });
 
+/**
+ * Date Default
+ * */
+function setDates() {
+
+    const date = new Date();
+
+    const day = ("0" + date.getDate()).slice(-2);
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+
+    const today = date.getFullYear() + "-" + (month) + "-" + (day);
+
+    $('#orderDate').val(today);
+
+}
+
 
