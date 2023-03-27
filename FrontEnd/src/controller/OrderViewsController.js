3d2808b32e6f7843fd34386f93978a1136fc84f6
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-27 12:21:41 +0530

    Add Order table controller

--- FrontEnd/src/controller/OrderViewsController.js
+++ FrontEnd/src/controller/OrderViewsController.js
@@ -4,5 +4,29 @@
  **/
 
 let baseUrlOrderDetails = "http://localhost:8080/BackEnd_war/";
+loadAllOrders();
 
+
+function loadAllOrders() {
+    $("#tblOrder").empty();
+    $.ajax({
+        url: baseUrlOrderDetails + "orders/LoadOrders", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
+
+            for (let i of res.data) {
+                let oid = i.oid;
+                let date = i.date;
+                let cusID = i.cusID;
+
+                let row = "<tr><td>" + oid + "</td><td>" + date + "</td><td>" + cusID + "</td></tr>";
+                $("#tblOrder").append(row);
+            }
+            console.log(res.message);
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message);
+        }
+
+    });
+}
 
