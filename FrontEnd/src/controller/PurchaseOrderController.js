c9fc936e3471e9c31bba171a3a7874d9c6c48fb9
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-27 12:26:56 +0530

    Add Order ID genatrated code

--- FrontEnd/src/controller/PurchaseOrderController.js
+++ FrontEnd/src/controller/PurchaseOrderController.js
@@ -8,3 +8,35 @@ let baseUrlPlaceOrder = "http://localhost:8080/BackEnd_war/";
 $("#btnPurchase").attr('disabled', true);
 $("#btnAddToCart").attr('disabled', true);
 
+/**
+ * Invoice Details
+ * */
+
+/**
+ * Invoice Details
+ * Order ID
+ * */
+function generateOrderID() {
+    $("#orderId").val("ODI-001");
+    $.ajax({
+        url: baseUrlPlaceOrder + "orders/OrderIdGenerate",
+        method: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let orderId = resp.value;
+            let tempId = parseInt(orderId.split("-")[1]);
+            tempId = tempId + 1;
+            if (tempId <= 9) {
+                $("#orderId").val("ODI-00" + tempId);
+            } else if (tempId <= 99) {
+                $("#orderId").val("ODI-0" + tempId);
+            } else {
+                $("#orderId").val("ODI-" + tempId);
+            }
+        },
+        error: function (ob, statusText, error) {
+
+        }
+    });
+}
